9f7b08f356efae62276ec02c6ae2aa7dee15a8ee
XYHOwen <160343500+XYHOwen@users.noreply.github.com>
2024-11-11 01:31:35 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 
 /*字符拼接区*/
-var introduce =' \/*\n  * Hi,亲爱的林茜小姐姐： \n  * 我们又见面啦！有没有很惊喜呀！ \n  * 不知不觉2024年已经开始了，一直都没有好好陪你～ \n  * 难得我有时间和闲心，很多话也不知道从何开口～ \n  * 我知道你一定会来到这里～ \n  * 那就让这个虚拟的独白就当作我们今年快乐时光的开始吧！ \n  * 有点冷清哎～那就先来点音乐吧～ \n  * 如果没有听见背景音乐请你轻点两下屏幕哦～ \n  * 你总让我用自己的方式表达爱意～ \n  * 那么作为一个程序猿,发挥程序猿的特长就送你张网页吧~ \n  * 请原谅我用这种敷衍的形式～  \n  * 好啦，就说这么多啦，那我就要开始啦～ \n  * 你那么爱美那我就先从网页的美化开始吧！\n */';
-	var sty1='\n  /* 那就先从背景开始吧～ 既然是仙女那就要粉粉的～ */ \n  body{\n\t background:-webkit-linear-gradient(#f54ea2,#ff7676);\n\t font-family:"微软雅黑";\n   }\n   /* 搬个凳子坐下来酝酿一下，哈哈哈，让代码也歇会儿～ */\n   #wrap{\n\t overflow:auto;\n\t width:40%;\n\t height:420px;\n\t border-radius:5px;\n\t box-shadow:0 0 15px #fff;\n\t font-size:14px;\n\t font-weight:bold;\n   }';
-	var trs1 = '\n  \/* 代码也很丑，那我也要让它变得漂亮起来～ *\/';
+var introduce =' \/*\n  * Hi,亲爱的Easy小仙女： \n  * 我们又见面啦！没错，我就是让你在哪里都能看到我留下的足迹，虽然这很幼稚！ \n  * 不知不觉2024年快要结束了，一直都没有好好陪你～ \n  * 很多话也不知道从何开口，那就偷偷记录在这里吧～ \n  * 原谅我出现的这么突然，也原谅我这么冒昧的打扰，嘿嘿～ \n  * 但是你说的嘛，勇敢的人先享受世界，那我肯定不会错过这个机会啦～ß \n  * 平时比较爱整理，偶然间翻出来的贺卡，想了好久才想起来是你送的，后来才断断续续的联系你 \n  * 还记得很久很久之前我就找过你，那时的你和现在一样，都很高冷，还记得那时候送你礼物你都不收 \n  * 随便你的名字有着随便的意思，但是你真的是一个很有原则的女孩纸 \n  * 那么多加分点，永远都闪耀着光点的仙女，当然我要努力去追寻你啦～ \n  * 虽然我在你眼里看来似乎不是最佳的选择，但我是一个不要脸的人 \n  * 勇气都是你给我的，那我这张本就不帅气的脸算什么呢，哈哈哈。不过好在似乎一切都是美好的 \n  * 那么，亲爱的李小姐，很高兴从新认识你，还请多多指教～\n */';
+var sty1='\n  /* 既然这样，那就正式一点，也请允许我卖弄一下，哈哈哈。颜色就由我来选吧，那就选粉色吧！ */ \n  body{\n\t background:-webkit-linear-gradient(#f54ea2,#ff7676);\n\t font-family:"微软雅黑";\n   }\n   /* 搬个凳子坐下来，虽然隔着屏幕，但还是有点小紧张和激动，我可能需要酝酿一下，哈哈哈哈，顺便让代码也歇会儿～ */\n   #wrap{\n\t overflow:auto;\n\t width:40%;\n\t height:420px;\n\t border-radius:5px;\n\t box-shadow:0 0 15px #fff;\n\t font-size:14px;\n\t font-weight:bold;\n   }';
+var trs1 = '\n  \/* 代码也很丑，那我也要让它变得漂亮起来～ *\/';
 var lightheight = '\n  #wrap{\n\t background:#002B36;\n\t color: #BE84F2;\n   }\n  .token.property{\n\t color:#64d5ea;\n   }\n   .token.comment{\n\t color:857F6B;\n   }\n';
-    var trs2 = '  \/* 现在我也酝酿的差不多啦～ \n  * 那就随便说点吧～先让代码到一边凉快一会～ *\/';
-    var scrollRight = '\n  #wrap{\n\t transform: rotateY(-10deg);\n\t position:fixed;\n\t left:53%;\n\t top: 4%;\n\t height: 85%\n   } \n   \/* 嗯～～～，还是正式一点吧～ \n  * 我准备一张纸，慢慢写下来吧～ \n  * 稍坐会儿哈，我马上就来～ *\/\n  ';
-var drawBoard = '#loveme{\n\t background: url(1.png);\n\t background-repeat: no-repeat;\n\t background-size: cover;\n\t background-position: center center;\n\t overflow:auto;\n\t position:fixed;\n\t top:40px;\n\t left:5%;\n\t width:45%;\n\t height:85%;\n\t color:#000;\n\t white-space: pre-wrap;\n\t box-shadow:0 0 15px #fff;\n\t border-radius:5px;\n\t transform:rotateY(10deg);\n  /* 久等啦～我来啦我的小公主～ */ \n   }';
-//var audio = new Audio("/Users/xyhowen/Desktop/LoveDebug/归档/MySoul.mp3");audio.play();
-var loveme = '\n<div><Temp>致林小姐的一封信</Temp></div><p>遇见你是一件特别美好、幸福开心的事情，一路上走过了多少磕磕绊绊。其中有过争吵，有过心酸，有过开心，有过难过，也有过悲伤，正式因为这些，才让我们彼此的感情更加深厚，才让我们更了解彼此！</p><h2>在长沙的时候</h2><p>还记得在长沙工作你刚来公司的时候,感觉你很高冷,没怎么说过话对你也不怎么了解,都不记得我们是怎么搭上话的,如果没记错的话应该是抖音让我们相识.</p><h2>最好的回忆</h2><p>还记得以前每周都会给你买酸奶一起分赃,呸不对,是一起分享.每次拿给你感觉你都挺不好意思的,我一直觉都得你是比较内向的女孩.第一次问你喜欢吃什么味道你说草莓,我以为你喜欢草莓以后就都是买的这一款,原来特喵的你并不是特别喜欢只是随便说的.尽管如此想想还是很好玩.</p><h2>零体验</h2><p>本来以为你是很正紧的,没想到那段期间我拿你当朋友,你竟然拿我当男友,贪图我的美色,哎呀说出来真是有点不太好意思.认识你之后我们从学英语到学韩剧学了各种各样的语言,掌握了多国的换算人民币汇率~感谢你带给我的欢乐.</p><h2>Ending</h2><div><p>You are my today and all of my tomorrows.<br>你是我的今天和所有的明天。</p></div>';
+var trs2 = '  \/* 现在我也酝酿的差不多啦～ \n  * 那我就即时发挥了哈 *\/';
+var scrollRight = '\n  #wrap{\n\t transform: rotateY(-10deg);\n\t position:fixed;\n\t left:53%;\n\t top: 4%;\n\t height: 85%\n   } \n   \/* 嗯～～～，还是正式一点吧～ \n  * 我准备一张纸，慢慢写下来吧～ \n  * 稍坐会儿哈，我马上就来～ *\/\n  ';
+var drawBoard = '#loveme{\n\t background: url(/Users/xyhowen/Desktop/love/1.png);\n\t background-repeat: no-repeat;\n\t background-size: cover;\n\t background-position: center center;\n\t overflow:auto;\n\t position:fixed;\n\t top:40px;\n\t left:5%;\n\t width:45%;\n\t height:85%;\n\t color:#000;\n\t white-space: pre-wrap;\n\t box-shadow:0 0 15px #fff;\n\t border-radius:5px;\n\t transform:rotateY(10deg);\n  /* 久等啦～我来啦～我的小公主～ */ \n   }';
+var loveme = '\n<div><Temp>致李小姐的一封信</Temp></div><p>或许是因为上天早已注定，让几百年不联系的我们再次相聚，这对我来说是一件特别美好、幸福开心的事情，或许之前我会不知所措，但是现在我至少有能力和底气站在的面前。能够呵护你，照顾你。虽然我们走过的时间并不是很长，其中有过开心快乐、难过伤心、也有过小争吵，但这都不会阻挡我靠近你的脚步！我不擅长能言善辩，也不能像那些有趣的朋友为你提供很多情绪价值，但坦诚相待和一只都在或许会成为我在你这里最优秀的品质！</p><h2>最好的回忆国庆假期</h2><p>勇敢的人先享受世界，这或许会是我受益匪浅的一句话，很开心与你一起度过的七天假期，这将是我们的开始，也将是我们美好回忆的开端。虽然你每次都说不喜欢出远门，但是你每天都陪着我走遍重庆的每一个景区，一起打卡，一起吃饭，一起抓娃娃，一起逛街买东西......穿个高跟鞋每天都走的很累很累，但第二天依旧不惧疼痛，不厌其烦的陪着我，这些点点滴滴都将是珍贵的记忆。这也是我靠近你的开始！</p><h2>很忙的我们</h2><p>你有一个很闪耀的点，那就是你很独立，有着很强的上进心和事业心。刚认识你那段时间，你每天都在为工作找工作，虽然你口头说着是为了赚钱，但是能看出来你和我一样也是一个闲不住的人。我都不止一次看到上班带给你的元气满满，哈哈哈。我也是一个工作比较忙的人，今年公司项目特别多，再加上晋升之后就更忙了，所以我们大多时间都是在忙彼此的工作。其实我是很担心的，我担心会淡，会远，当然啦，这还要看我怎么去经营这段感情了。有时候很想找你，但是我这个撩不动你的大直男每次都不知道说什么。说来也是我自己的一大缺点，哈哈哈。不过放心啦，我会用我的方式去守护这段来之不易的感情，即使很平淡，只要最后站在一起的是我们就好啦～ </p><h2>零体验</h2><p>突然的出现导致你不止一次问我我是不是坏人，哈哈哈。其实有时候我就在想，像你这么高冷的女生，那要什么样的坏人才能够把你拐走呢？什么用情至深，金钱诱惑你都对他们有着极强的抵抗力，哈哈哈哈。有的时候感觉你看人看物的眼力劲比我都还要好。你也不止一次的问过我我到底是一个什么样的人，我要找一个什么样的人，我们都给过彼此答案，但那只是片面。长久的人设还是需要长时间相处才知道，才了解，所以这就是最初我为什么不直面告诉你的原因，因为我也害怕我说不清楚，亦或者人不对版，哈哈哈哈。所以我都没有记着找你要答案，因为我知道我们还有很长的路要走，真正的长跑或许现在还未开始。你给我说你是慢热的人，但我的回应也是如此，我永远都不着急要你的答复，只要一直陪在身边，多晚都没关系，多晚我都在，一直都在！</p><h2>Ending</h2><div><p>You are my today and all of my tomorrows.<br>你是我的今天和所有的明天。</p></div>';
 var trs3 = ' \n   \/* 嗯～～好像还是不太好看！让我再微调一下吧～ *\/\n   #loveme h2{\n\t display: inline-block;\n\t border-bottom: 1px solid;\n\t margin: 1em 0 .5em;\n\t font-size: 15px;\n\t font-weight: 700;\n\t line-height: 2;\n\t font-family: microsoft yahei;\n   }\n   #loveme p{\n\t line-height: 1.5; padding: 1em;  }\n   #loveme div p{\n\t margin: 1em;\n\t padding: 1.5em;\n\t background: rgba(221,221,221,.5);\n   } \n  #loveme div Temp{\n\t font-size: 20px; text-align:center ;\n\t padding: .5em;\n\t background: rgba(255,0,0,.5);\n   }\n\t\/*美感不怎么滴,只能这样了~*/';/* *** 最后为大家带来一个小惊喜，倒数三个数  \n\t* 3\n\t* 2\n\t* 1\n\t*** */
-	var str = introduce.concat(sty1).concat(trs1).concat(lightheight).concat(trs2).concat(scrollRight).concat(drawBoard).concat(loveme).concat(trs3);
+var str = introduce.concat(sty1).concat(trs1).concat(lightheight).concat(trs2).concat(scrollRight).concat(drawBoard).concat(loveme).concat(trs3);
 
 
 	var styleTag = document.getElementById('styleTag');
 	var sourceBoard = document.getElementById('wrap');
 	//var Mytemp = document.getElementById('Temp');
 	var n = 0;
-	var No_One = 500 ;  //Scroll_Start 开始滚动
-	var highlight_No = 650 ; //代码高亮起始
-	var languagesJudge_No = 1278 ;//独白结束
-	var languages_No = 1362 ; //独白开始，代码结束
-	var languageTotal_No = 1951 ;  //总结束
-	var SpellSpeed = 120 ;
+	var No_One = 520 ;  //Scroll_Start 开始滚动
+	var highlight_No = 906 ; //代码高亮起始
+	var languagesJudge_No = 1300 ;//独白结束
+	var languages_No = 1599 ; //独白开始，代码结束
+	var languageTotal_No = 2799 ;  //总结束
+	var SpellSpeed = 1 ;
 
 	//Mytemp.style.textAlign = "Center" ;
 	const MySoulAudio = document.getElementById('MySoul');
@@ -31,7 +30,7 @@ var trs3 = ' \n   \/* 嗯～～好像还是不太好看！让我再微调一下�
 	// if (n>languages_No){
 	// 	audio.play();
 	// }
-	
+
 	// button.addEventListener('click',(Test) => {
 	// 	Audio.play();
 	// });
@@ -42,7 +41,7 @@ var controller= setInterval(put,SpellSpeed);
 
 			n++;
 
-			if (n>=125){
+			if (n>=145){
 				AutumnMissAudio.play();
 				//SpellSpeed = 80 ;
 				}
@@ -83,7 +82,7 @@ var controller= setInterval(put,SpellSpeed);
 			if (n>languages_No){
 				MySoulAudio.play();
 				AutumnMissAudio.pause();
-				SpellSpeed = 520 ;
+				//SpellSpeed = 10 ;
 				}
 
 			if(n>languages_No && n<languageTotal_No && AutumnMissAudio.paused){
