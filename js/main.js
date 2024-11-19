79d3286b16361ae7fa820fe27ce58186b8f1feee
XYHOwen <160343500+XYHOwen@users.noreply.github.com>
2024-11-20 00:26:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var trs1 = '\n  \/* 代码也很丑，那我也要让它变得漂亮起来～ *\
 var lightheight = '\n  #wrap{\n\t background:#002B36;\n\t color: #BE84F2;\n   }\n  .token.property{\n\t color:#64d5ea;\n   }\n   .token.comment{\n\t color:857F6B;\n   }\n';
 var trs2 = '  \/* 现在我也酝酿的差不多啦～ \n  * 那我就即时发挥了哈 *\/';
 var scrollRight = '\n  #wrap{\n\t transform: rotateY(-10deg);\n\t position:fixed;\n\t left:53%;\n\t top: 4%;\n\t height: 85%\n   } \n   \/* 嗯～～～，还是正式一点吧～ \n  * 我准备一张纸，慢慢写下来吧～ \n  * 稍坐会儿哈，我马上就来～ *\/\n  ';
-var drawBoard = '#loveme{\n\t background: url(/Users/xyhowen/Desktop/love/1.png);\n\t background-repeat: no-repeat;\n\t background-size: cover;\n\t background-position: center center;\n\t overflow:auto;\n\t position:fixed;\n\t top:40px;\n\t left:5%;\n\t width:45%;\n\t height:85%;\n\t color:#000;\n\t white-space: pre-wrap;\n\t box-shadow:0 0 15px #fff;\n\t border-radius:5px;\n\t transform:rotateY(10deg);\n  /* 久等啦～我来啦～我的小公主～ */ \n   }';
-var loveme = '\n<div><Temp>致李小姐的一封信</Temp></div><p>或许是因为上天早已注定，让几百年不联系的我们再次相聚，这对我来说是一件特别美好、幸福开心的事情，或许之前我会不知所措，但是现在我至少有能力和底气站在的面前。能够呵护你，照顾你。虽然我们走过的时间并不是很长，其中有过开心快乐、难过伤心、也有过小争吵，但这都不会阻挡我靠近你的脚步！我不擅长能言善辩，也不能像那些有趣的朋友为你提供很多情绪价值，但坦诚相待和一只都在或许会成为我在你这里最优秀的品质！</p><h2>最好的回忆国庆假期</h2><p>勇敢的人先享受世界，这或许会是我受益匪浅的一句话，很开心与你一起度过的七天假期，这将是我们的开始，也将是我们美好回忆的开端。虽然你每次都说不喜欢出远门，但是你每天都陪着我走遍重庆的每一个景区，一起打卡，一起吃饭，一起抓娃娃，一起逛街买东西......穿个高跟鞋每天都走的很累很累，但第二天依旧不惧疼痛，不厌其烦的陪着我，这些点点滴滴都将是珍贵的记忆。这也是我靠近你的开始！</p><h2>很忙的我们</h2><p>你有一个很闪耀的点，那就是你很独立，有着很强的上进心和事业心。刚认识你那段时间，你每天都在为工作找工作，虽然你口头说着是为了赚钱，但是能看出来你和我一样也是一个闲不住的人。我都不止一次看到上班带给你的元气满满，哈哈哈。我也是一个工作比较忙的人，今年公司项目特别多，再加上晋升之后就更忙了，所以我们大多时间都是在忙彼此的工作。其实我是很担心的，我担心会淡，会远，当然啦，这还要看我怎么去经营这段感情了。有时候很想找你，但是我这个撩不动你的大直男每次都不知道说什么。说来也是我自己的一大缺点，哈哈哈。不过放心啦，我会用我的方式去守护这段来之不易的感情，即使很平淡，只要最后站在一起的是我们就好啦～ </p><h2>零体验</h2><p>突然的出现导致你不止一次问我我是不是坏人，哈哈哈。其实有时候我就在想，像你这么高冷的女生，那要什么样的坏人才能够把你拐走呢？什么用情至深，金钱诱惑你都对他们有着极强的抵抗力，哈哈哈哈。有的时候感觉你看人看物的眼力劲比我都还要好。你也不止一次的问过我我到底是一个什么样的人，我要找一个什么样的人，我们都给过彼此答案，但那只是片面。长久的人设还是需要长时间相处才知道，才了解，所以这就是最初我为什么不直面告诉你的原因，因为我也害怕我说不清楚，亦或者人不对版，哈哈哈哈。所以我都没有记着找你要答案，因为我知道我们还有很长的路要走，真正的长跑或许现在还未开始。你给我说你是慢热的人，但我的回应也是如此，我永远都不着急要你的答复，只要一直陪在身边，多晚都没关系，多晚我都在，一直都在！</p><h2>Ending</h2><div><p>You are my today and all of my tomorrows.<br>你是我的今天和所有的明天。</p></div>';
-var trs3 = ' \n   \/* 嗯～～好像还是不太好看！让我再微调一下吧～ *\/\n   #loveme h2{\n\t display: inline-block;\n\t border-bottom: 1px solid;\n\t margin: 1em 0 .5em;\n\t font-size: 15px;\n\t font-weight: 700;\n\t line-height: 2;\n\t font-family: microsoft yahei;\n   }\n   #loveme p{\n\t line-height: 1.5; padding: 1em;  }\n   #loveme div p{\n\t margin: 1em;\n\t padding: 1.5em;\n\t background: rgba(221,221,221,.5);\n   } \n  #loveme div Temp{\n\t font-size: 20px; text-align:center ;\n\t padding: .5em;\n\t background: rgba(255,0,0,.5);\n   }\n\t\/*美感不怎么滴,只能这样了~*/';/* *** 最后为大家带来一个小惊喜，倒数三个数  \n\t* 3\n\t* 2\n\t* 1\n\t*** */
+var drawBoard = '#loveme{\n\t background: url(https://s21.ax1x.com/2024/11/20/pAWlZzn.png);\n\t background-repeat: no-repeat;\n\t background-size: cover;\n\t background-position: center center;\n\t overflow:auto;\n\t position:fixed;\n\t top:40px;\n\t left:5%;\n\t width:45%;\n\t height:85%;\n\t color:#000;\n\t white-space: pre-wrap;\n\t box-shadow:0 0 15px #fff;\n\t border-radius:5px;\n\t transform:rotateY(10deg);\n  /* 久等啦～我来啦～我的小公主～ */ \n   }';
+var loveme = '\n<div><Temp>致李小姐的一封信</Temp></div><p>或许是因为上天早已注定，让几百年不联系的我们再次相聚，这对我来说是一件特别美好、幸福开心的事情，或许之前我会不知所措，但是现在我至少有能力和底气站在的面前。能够呵护你，照顾你。虽然我们走过的时间并不是很长，其中有过开心快乐、难过伤心、也有过小争吵，但这都不会阻挡我靠近你的脚步！我不擅长能言善辩，也不能像那些有趣的朋友为你提供很多情绪价值，但坦诚相待和一只都在或许会成为我在你这里最优秀的品质！</p><h2>最美回忆</h2><p>勇敢的人先享受世界，这或许会是我受益匪浅的一句话，很开心与你一起度过的七天假期，这将是我们的开始，也将是我们美好回忆的开端。虽然你每次都说不喜欢出远门，但是你每天都陪着我走遍重庆的每一个景区，一起打卡，一起吃饭，一起抓娃娃，一起逛街买东西......穿个高跟鞋每天都走的很累很累，但第二天依旧不惧疼痛，不厌其烦的陪着我，这些点点滴滴都将是珍贵的记忆。这也是我靠近你的开始！</p><h2>忙碌的我们</h2><p>你有一个很闪耀的点，那就是你很独立，有着很强的上进心和事业心。刚认识你那段时间，你每天都在为工作找工作，虽然你口头说着是为了赚钱，但是能看出来你和我一样也是一个闲不住的人。我都不止一次看到上班带给你的元气满满，哈哈哈。我也是一个工作比较忙的人，今年公司项目特别多，再加上晋升之后就更忙了，所以我们大多时间都是在忙彼此的工作。其实我是很担心的，我担心会淡，会远，当然啦，这还要看我怎么去经营这段感情了。有时候很想找你，但是我这个撩不动你的大直男每次都不知道说什么。说来也是我自己的一大缺点，哈哈哈。不过放心啦，我会用我的方式去守护这段来之不易的感情，即使很平淡，只要最后站在一起的是我们就好啦～ </p><h2>零体验</h2><p>突然的出现导致你不止一次问我我是不是坏人，哈哈哈。其实有时候我就在想，像你这么高冷的女生，那要什么样的坏人才能够把你拐走呢？什么用情至深，金钱诱惑你都对他们有着极强的抵抗力，哈哈哈哈。有的时候感觉你看人看物的眼力劲比我都还要好。你也不止一次的问过我我到底是一个什么样的人，我要找一个什么样的人，我们都给过彼此答案，但那只是片面。长久的人设还是需要长时间相处才知道，才了解，所以这就是最初我为什么不直面告诉你的原因，因为我也害怕我说不清楚，亦或者人不对版，哈哈哈哈。所以我都没有记着找你要答案，因为我知道我们还有很长的路要走，真正的长跑或许现在还未开始。你给我说你是慢热的人，但我的回应也是如此，我永远都不着急要你的答复，只要一直陪在身边，多晚都没关系，多晚我都在，一直都在！</p><h2>Ending</h2><div><p>幸得识卿桃花面，从此阡陌多暖春。<br>愿我如星如君，夜夜流光相皎洁。</p></div>';
+var trs3 = ' \n   \/* 嗯～～好像还是不太好看！让我再微调一下吧～ *\/\n   #loveme h2{\n\t display: inline-block;\n\t border-bottom: 1px solid;\n\t margin: 1em 0 .5em;\n\t font-size: 15px;\n\t font-weight: 700;\n\t line-height: 2;\n\t font-family: microsoft yahei;\n\t color: red ; }\n   #loveme p{\n\t line-height: 1.5; padding: 1em; }\n   #loveme div p{\n\t font-size:18px ;\n\t line-height: 40px ; \n\t color:purple ;\n\t padding: .5em;\n\t display: flex; \n\t justify-content: center; \n\t background: rgba(255,221,255,.5);\n\t } \n  #loveme div Temp{\n\t font-size: 30px; \n\t display: flex; \n\t justify-content: center; \n\t padding-top: 1em;\n\t }\n\t\/*美感不怎么滴,只能这样了~*/';/* *** 最后为大家带来一个小惊喜，倒数三个数  \n\t* 3\n\t* 2\n\t* 1\n\t*** */
 var str = introduce.concat(sty1).concat(trs1).concat(lightheight).concat(trs2).concat(scrollRight).concat(drawBoard).concat(loveme).concat(trs3);
 
 
@@ -17,11 +17,11 @@ var str = introduce.concat(sty1).concat(trs1).concat(lightheight).concat(trs2).c
 	//var Mytemp = document.getElementById('Temp');
 	var n = 0;
 	var No_One = 520 ;  //Scroll_Start 开始滚动
-	var highlight_No = 906 ; //代码高亮起始
-	var languagesJudge_No = 1300 ;//独白结束
-	var languages_No = 1599 ; //独白开始，代码结束
-	var languageTotal_No = 2799 ;  //总结束
-	var SpellSpeed = 150 ;
+	var highlight_No = 1106 ; //代码高亮起始
+	var languagesJudge_No = 1100 ;//独白结束
+	var languages_No = 1598 ; //独白开始，代码结束
+	var languageTotal_No = 2787 ;  //总结束
+	var SpellSpeed = 100 ;
 
 	//Mytemp.style.textAlign = "Center" ;
 	const MySoulAudio = document.getElementById('MySoul');
@@ -41,7 +41,7 @@ var controller= setInterval(put,SpellSpeed);
 
 			n++;
 
-			if (n>=145){
+			if (n>=0){
 				AutumnMissAudio.play();
 				//SpellSpeed = 80 ;
 				}
@@ -82,7 +82,7 @@ var controller= setInterval(put,SpellSpeed);
 			if (n>languages_No){
 				MySoulAudio.play();
 				AutumnMissAudio.pause();
-				SpellSpeed = 500 ;
+				//SpellSpeed = 10 ;
 				}
 
 			if(n>languages_No && n<languageTotal_No && AutumnMissAudio.paused){
